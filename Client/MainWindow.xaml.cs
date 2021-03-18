1cab4a7cad94d0e4db1bb7b0a693327ebe061e11
bjoonly <elumnaya@gmail.com>
2021-03-18 18:57:41 +0200

    Init

---
+++ Client/MainWindow.xaml.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml.Serialization;
+
+namespace Client
+{
+    [Serializable]
+    public class MessageInfo
+    {
+        public string Nickname { get; set; }
+        public string IPAddress { get; set; }
+        public string Time { get; set; }
+        public string Text { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        
+        private static string remoteIPAddress = "127.0.0.1";
+       public string nickname;
+        private static int remotePort = 8080;
+
+        UdpClient client = new UdpClient(0);
+
+        ObservableCollection<MessageInfo> messages = new ObservableCollection<MessageInfo>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            list.ItemsSource = messages;
+
+            Task.Run(() => Listen());
+        }
+
+        private void Listen()
+        {
+            IPEndPoint iPEndPoint = null;
+            while (true)
+            {
+                try
+                {
+                    byte[] bytes = client.Receive(ref iPEndPoint);
+                    XmlSerializer xml = new XmlSerializer(typeof(MessageInfo));
+                    MemoryStream stream = new MemoryStream();
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Position = 0;
+                    var mi = (MessageInfo)xml.Deserialize(stream);
+                    App.Current.Dispatcher.Invoke(new Action(() =>
+                    {
+                        if (mi.Text.Contains(nickname) && mi.Text.ToLower().Contains("join"))
+                        {
+                            nicknameTB.IsEnabled = false;
+                            joinButton.IsEnabled = false;
+                            messageTB.IsEnabled = true;
+                            leaveButton.IsEnabled = true;
+                            sendButton.IsEnabled = true;
+                        }
+                    }
+                    ));
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        messages.Insert(0, new MessageInfo()
+                        {
+                            IPAddress = mi.IPAddress,
+                            Time = mi.Time,
+                            Text = mi.Text,
+                            Nickname=mi.Nickname
+                        });
+                    }));
+                    stream.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        private void SendMessage(string msg)
+        {
+            if (string.IsNullOrWhiteSpace(msg)) return;
+
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Parse(remoteIPAddress), remotePort);
+            XmlSerializer xml = new XmlSerializer(typeof(MessageInfo));
+            MessageInfo mi = new MessageInfo() { Text = msg, Time = DateTime.Now.ToShortTimeString(), Nickname = nickname, IPAddress = remoteIPAddress.ToString() };
+            MemoryStream stream = new MemoryStream();
+            xml.Serialize(stream, mi);
+            stream.Position = 0;
+            byte[] bytes = new byte[stream.Length];
+            stream.Read(bytes, 0, (int)stream.Length);
+            client.Send(bytes, bytes.Length, iPEndPoint);
+        
+        }
+
+        private void joinButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (String.IsNullOrEmpty(nicknameTB.Text))
+            {
+                MessageBox.Show("Enter your nickname.");
+                return;
+            }
+            nickname = nicknameTB.Text;
+            SendMessage("<JOIN>");
+            
+               
+           
+                    
+           
+           
+        
+        }
+
+        private void leaveButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<LEAVE>");
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                messages.Insert(0, new MessageInfo()
+                {
+                    IPAddress = remoteIPAddress,
+                    Time = DateTime.Now.ToShortTimeString(),
+                    Text = $"{nickname} left the chat.",
+                    Nickname = nickname
+                });
+            }));
+           
+            nicknameTB.IsEnabled = true;
+            joinButton.IsEnabled = true;
+            messageTB.IsEnabled = false;
+            leaveButton.IsEnabled = false;
+            sendButton.IsEnabled = false;
+        }
+
+        private void sendButton_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage(messageTB.Text);
+        }
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            SendMessage("<LEAVE>");
+            client.Close();
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Server
+{
+    [Serializable]
+    public class MessageInfo
+    {
+        public string Nickname { get; set; }
+        public string IPAddress { get; set; }
+        public string Time { get; set; }
+        public string Text { get; set; }
+    }
+    class Program
+    {
+        private const int port = 8080;
+        private static List<IPEndPoint> members = new List<IPEndPoint>();
+        static void Main(string[] args)
+        {
+        Semaphore semaphore = new Semaphore(2, 2);
+            UdpClient server = new UdpClient(port);
+            IPEndPoint groupEP = null;
+
+            try
+            {
+                while (true)
+                {
+                    Console.WriteLine("\tWaiting for a message...");
+                    byte[] bytes = server.Receive(ref groupEP);
+
+                    XmlSerializer xml = new XmlSerializer(typeof(MessageInfo));
+                    MemoryStream stream = new MemoryStream();
+                    stream.Write(bytes, 0, bytes.Length);
+                    stream.Position = 0;
+                    var mi = (MessageInfo)xml.Deserialize(stream);
+                    mi.IPAddress = groupEP.ToString();
+                    bool isSuccesful;
+                    if (mi.Text == "<JOIN>")
+                    {
+                        if (!semaphore.WaitOne(500))
+                        {
+                            MessageInfo info = new MessageInfo() { Nickname = mi.Nickname, Text = "The maximum number of users has been reached. Try to join later.", IPAddress = mi.IPAddress, Time = mi.Time };
+                            XmlSerializer serializer = new XmlSerializer(typeof(MessageInfo));
+
+                            MemoryStream ms = new MemoryStream();
+                            xml.Serialize(ms, info);
+                            ms.Position = 0;
+                            byte[] minfo = new byte[ms.Length];
+                            ms.Read(minfo, 0, (int)ms.Length);
+                            server.Send(minfo, minfo.Length, groupEP);
+                           
+                           
+                            continue;
+                        }
+                        isSuccesful = AddMember(groupEP);
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine($"Request to connect {mi.Nickname} at {DateTime.Now.ToShortTimeString()}\n");
+                        
+                        if (isSuccesful)
+                        {
+                            Console.WriteLine($"Operation completed succesful!\n");
+                           
+                            foreach (var m in members)
+                            {
+                                try
+                                {
+                                    MessageInfo info = new MessageInfo() { Nickname = mi.Nickname, Text = $"{mi.Nickname} has joined to chat.", IPAddress = mi.IPAddress, Time = mi.Time };
+                                    XmlSerializer serializer = new XmlSerializer(typeof(MessageInfo));
+
+                                    MemoryStream ms = new MemoryStream();
+                                    xml.Serialize(ms, info);
+                                    ms.Position = 0;
+                                    byte[] minfo = new byte[ms.Length];
+                                    ms.Read(minfo, 0, (int)ms.Length);
+                                    server.Send(minfo, minfo.Length, m);
+
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                                }
+                            }
+                        }
+                    }
+                    else if (mi.Text == "<LEAVE>")
+                    {
+                        isSuccesful = RemoveMember(groupEP);
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.WriteLine($"Request to leave from {mi.Nickname} at {DateTime.Now.ToShortTimeString()}\n");
+                        if (isSuccesful)
+                        {
+                            Console.WriteLine($"Operation completed succesful!\n");
+                                   
+
+                            foreach (var m in members)
+                            {
+                                
+                                try
+                                {
+                                    MessageInfo info = new MessageInfo() { Nickname = mi.Nickname, Text = $"{mi.Nickname} left the chat.", IPAddress = mi.IPAddress, Time = mi.Time };
+                                    XmlSerializer serializer = new XmlSerializer(typeof(MessageInfo));
+                                 
+                                    MemoryStream ms = new MemoryStream();
+                                    xml.Serialize(ms, info);
+                                    ms.Position = 0;
+                                    byte[] minfo = new byte[ms.Length];
+                                    ms.Read(minfo, 0, (int)ms.Length);
+                                    server.Send(minfo,minfo.Length, m);
+                                }
+                                catch (Exception ex)
+                                {
+                                    Console.ForegroundColor = ConsoleColor.Red;
+                                    Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                                }
+                            }
+                            semaphore.Release();
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($"Message from {mi.Nickname} at {DateTime.Now.ToShortTimeString()}: {mi.Text}\n");
+                        foreach (var m in members)
+                        {
+                            try
+                            {                               
+                                server.Send(bytes, bytes.Length, m);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine($"Error with {m}: {ex.Message}\n");
+                            }
+                        }
+                    }
+                    Console.ResetColor();
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+            finally
+            {
+              
+                server.Close();
+            }
+        }
+
+        static bool AddMember(IPEndPoint endPoint)
+        {
+            var member = members.FirstOrDefault(m => m.ToString() == endPoint.ToString());
+            if (member == null)
+            {
+                members.Add(endPoint);
+                return true;
+            }
+            return false;
+        }
+        static bool CheckMember(IPEndPoint endPoint)
+        {
+            var member = members.FirstOrDefault(m => m.ToString() == endPoint.ToString());
+            if (member != null)
+            {              
+                return true;
+            }
+            return false;
+        }
+        static bool RemoveMember(IPEndPoint endPoint)
+        {
+            var member = members.FirstOrDefault(m => m.ToString() == endPoint.ToString());
+            if (member != null)
+            {
+                members.Remove(member);
+                return true;
+            }
+            return false;
+        }
+    }
+}
